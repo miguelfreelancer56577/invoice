d55cb06240258c6b4ee1c1410d0dba3b325623e8
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-05-26 14:02:53 -0500

    this class creates a xml report

--- invoice-report/src/main/java/com/mangelt/mx/invoice/report/App.java
+++ invoice-report/src/main/java/com/mangelt/mx/invoice/report/App.java
@@ -1,5 +1,15 @@
 package com.mangelt.mx.invoice.report;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FilenameUtils;
+
+import com.mangelt.mx.reader.Reader;
+import com.mangelt.mx.reader.impl.ReaderImpl;
+
 /**
  * Hello world!
  *
@@ -8,6 +18,53 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+        	
+        	List<File> invoices = new ArrayList<File>();  
+        	
+        	File dir = new File("C:\\Users\\vn0x53q\\Documents\\SAT\\facturas may-jun");
+        	
+//        	It gets all files 
+        	File[] files = dir.listFiles();
+        	
+//        	it verifies each files 
+        	for (File file : files) {
+        		
+//        		it checks if the file is a directory
+				if(file.isDirectory()){
+
+//					it gets all files into of the directory  
+					File[] concurrentFiles = file.listFiles();
+					
+//					it verifies each file
+					for (File file2 : concurrentFiles) {
+						
+//						it gets the file extension
+						String ext = FilenameUtils.getExtension(file2.getName());
+						
+//						if it is an xml file then is added
+						if(ext.equals("xml")){
+							invoices.add(file2);
+						}
+						
+					}
+					
+				}
+				
+			}
+			
+			Reader reader = new ReaderImpl();
+        	
+			StatementInvoices statementInvoices= new StatementInvoices("C:\\Users\\vn0x53q\\Documents\\SAT\\report.xls");
+			
+			statementInvoices.createHeadInXls();
+			
+			statementInvoices.createBodyXls(reader.readInvoice(invoices));
+			
+			statementInvoices.createFileXls();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 }
