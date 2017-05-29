ef03783e8fd714fa9142c96c37d5ad6596cf2e61
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-05-29 09:57:16 -0500

    I have created the cells and rows to show the information.

--- invoice-report/src/main/java/com/mangelt/mx/invoice/report/StatementInvoices.java
+++ invoice-report/src/main/java/com/mangelt/mx/invoice/report/StatementInvoices.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 
 import com.mangelt.mx.reader.api.Comprobante;
+import com.mangelt.mx.reader.api.Traslado;
 
 public class StatementInvoices extends WorkBookXls {
 	
@@ -25,16 +26,16 @@ public class StatementInvoices extends WorkBookXls {
 		
 		HSSFRow rowhead = sheet.createRow((short)positionRow);
 		rowhead.createCell(0).setCellValue("Folio Fiscal");
-		rowhead.createCell(0).setCellValue("RFC Emisor");
-		rowhead.createCell(0).setCellValue("Nombre o Razón Social del Emisor");
-		rowhead.createCell(0).setCellValue("RFC Receptor");
-		rowhead.createCell(0).setCellValue("Nombre o Razón Social del Receptor");
-		rowhead.createCell(0).setCellValue("Fecha de Emisión");
-		rowhead.createCell(0).setCellValue("Iva");
-		rowhead.createCell(0).setCellValue("monto");
-		rowhead.createCell(0).setCellValue("exento");
-		rowhead.createCell(0).setCellValue("Total");
-		rowhead.createCell(0).setCellValue("Efecto del Comprobante");
+		rowhead.createCell(1).setCellValue("RFC Emisor");
+		rowhead.createCell(2).setCellValue("Nombre o Razón Social del Emisor");
+		rowhead.createCell(3).setCellValue("RFC Receptor");
+		rowhead.createCell(4).setCellValue("Nombre o Razón Social del Receptor");
+		rowhead.createCell(5).setCellValue("Fecha de Emisión");
+		rowhead.createCell(6).setCellValue("Iva");
+		rowhead.createCell(7).setCellValue("monto");
+		rowhead.createCell(8).setCellValue("exento");
+		rowhead.createCell(9).setCellValue("Total");
+		rowhead.createCell(10).setCellValue("Efecto del Comprobante");
 		
 		positionRow++;
 		
@@ -46,17 +47,30 @@ public class StatementInvoices extends WorkBookXls {
 		for (Comprobante comprobante : invoices) {
 			
 			HSSFRow rowhead = sheet.createRow((short)positionRow);
-			rowhead.createCell(0).setCellValue("Folio Fiscal");
-			rowhead.createCell(0).setCellValue("RFC Emisor");
-			rowhead.createCell(0).setCellValue("Nombre o Razón Social del Emisor");
-			rowhead.createCell(0).setCellValue("RFC Receptor");
-			rowhead.createCell(0).setCellValue("Nombre o Razón Social del Receptor");
-			rowhead.createCell(0).setCellValue("Fecha de Emisión");
-			rowhead.createCell(0).setCellValue("Iva");
-			rowhead.createCell(0).setCellValue("monto");
-			rowhead.createCell(0).setCellValue("exento");
-			rowhead.createCell(0).setCellValue("Total");
-			rowhead.createCell(0).setCellValue("Efecto del Comprobante");
+			rowhead.createCell(0).setCellValue(comprobante.getComplemento().getTimbreFiscalDigital().getUUID());
+			rowhead.createCell(1).setCellValue(comprobante.getEmisor().getRfc());
+			rowhead.createCell(2).setCellValue(comprobante.getEmisor().getNombre());
+			rowhead.createCell(3).setCellValue(comprobante.getReceptor().getRfc());
+			rowhead.createCell(4).setCellValue(comprobante.getReceptor().getNombre());
+			rowhead.createCell(5).setCellValue(comprobante.getFecha().toString());
+			
+			try {
+				List<Traslado> impuestos = comprobante.getImpuestos().getTraslados().getTraslado();
+				
+				for (Traslado traslado : impuestos) {
+					if(traslado.getImpuesto().equalsIgnoreCase("Iva")){
+						rowhead.createCell(6).setCellValue(traslado.getImporte());
+						break;
+					}
+				}
+			} catch (Exception e) {
+				rowhead.createCell(6).setCellValue(0);
+			}
+//			
+			rowhead.createCell(7).setCellValue("monto");
+			rowhead.createCell(8).setCellValue("exento");
+			rowhead.createCell(9).setCellValue("Total");
+			rowhead.createCell(10).setCellValue(comprobante.getTipoDeComprobante());
 			
 			positionRow++;
 		}
