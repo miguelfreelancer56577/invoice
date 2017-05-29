9793382130c8f73be1f1c67ebb68601561ebaf64
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-05-29 12:55:35 -0500

    I have created a new object to create the report

--- invoice-report/src/main/java/com/mangelt/mx/invoice/report/App.java
+++ invoice-report/src/main/java/com/mangelt/mx/invoice/report/App.java
@@ -18,63 +18,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        try {
-        	
-        	List<File> invoices = new ArrayList<File>();  
-        	
-        	File dir = new File("C:\\Users\\vn0x53q\\Documents\\SAT\\facturas may-jun");
-        	
-//        	It gets all files 
-        	File[] files = dir.listFiles();
-        	
-//        	it verifies each files 
-        	for (File file : files) {
-        		
-//        		it checks if the file is a directory
-				if(file.isDirectory()){
-
-//					it gets all files into of the directory  
-					File[] concurrentFiles = file.listFiles();
-					
-//					it verifies each file
-					for (File file2 : concurrentFiles) {
-						
-//						it gets the file extension
-						String ext = FilenameUtils.getExtension(file2.getName());
-						
-//						if it is an xml file then is added
-						if(ext.equals("xml")){
-							invoices.add(file2);
-						}
-						
-					}
-					
-				}else{
-					
-//					it gets the file extension
-					String ext = FilenameUtils.getExtension(file.getName());
-					
-//					if it is an xml file then is added
-					if(ext.equals("xml")){
-						invoices.add(file);
-					}
-					
-				}
-				
-			}
-			
-			Reader reader = new ReaderImpl();
-        	
-			StatementInvoices statementInvoices= new StatementInvoices("C:\\Users\\vn0x53q\\Documents\\SAT\\report.xls");
-			
-			statementInvoices.createHeadInXls();
-			
-			statementInvoices.createBodyXls(reader.readInvoice(invoices));
-			
-			statementInvoices.createFileXls();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    	
+    	Report report = new Report();
+    	
+    	boolean result = report.doCreate("C:\\Users\\vn0x53q\\Documents\\SAT\\facturas may-jun");
+    	
+    	System.out.println(result);
+    	
     }
 }
