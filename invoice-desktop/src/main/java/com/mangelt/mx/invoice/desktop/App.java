b41633917be2365d26b6b5e20f3f0d620826c113
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-05-29 13:07:44 -0500

    It was created a new modulo to run the application

---
+++ invoice-desktop/src/main/java/com/mangelt/mx/invoice/desktop/App.java
@@ -0,0 +1,13 @@
+package com.mangelt.mx.invoice.desktop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
